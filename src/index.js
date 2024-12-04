849fdc5c89bee4e3951fa47eb8996c63995696cc
DaisyGaze <ameliasteyn03@gmail.com>
2024-12-04 14:12:08 +0200

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("hello world");
+function generatePoem(event) {
+  event.preventDefault();
+  let poemElement = document.querySelector("#poem");
+  new Typewriter("#poem", {
+    strings:
+      "Demain, dès l’aube, à l’heure où blanchit la campagne, Je partirai.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
